9490c0948e4dcae2dc3c513d1b937d6a6b9d8ba4
James Coll <james.coll@visma.com>
2018-03-11 20:04:07 +0000

    adding v2 mobile wireframe

--- mobile/ijustspent/App.js
+++ mobile/ijustspent/App.js
@@ -1,23 +1,72 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default class App extends React.Component {
-  render() {
+import {
+  ListView,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+
+import styles from './styles';
+
+export default class TasksList extends Component {
+  constructor (props) {
+    super (props);
+
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.state = {
+      ds: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      }),
+      listOfTasks: [],
+      text: ''
+    };
+  }
+
+  render () {
+    const dataSource = this.state.ds.cloneWithRows(this.state.listOfTasks);
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={ styles.container }>
+        <TextInput
+          autoCorrect={ false }
+          onChangeText={ (text) => this._changeTextInputValue(text) }
+          onSubmitEditing={ () => this._addTask() }
+          returnKeyType={ 'done' }
+          style={ styles.textInput }
+          value={ this.state.text }
+        />
+        <ListView
+          dataSource={ dataSource }
+          enableEmptySections={ true }
+          renderRow={ (rowData) => this._renderRowData(rowData) }
+        />
       </View>
     );
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  _addTask () {
+    const newListOfTasks = [...this.state.listOfTasks, this.state.text];
+
+    this.setState({
+      listOfTasks: newListOfTasks
+    });
+
+    this._changeTextInputValue('');
+  }
+
+  _changeTextInputValue (text) {
+    this.setState({
+      text: text
+    });
+  }
+
+  _renderRowData (rowData) {
+    return (
+      <Text>{ rowData }</Text>
+    )
+  }
+}
